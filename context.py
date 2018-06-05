ba5e802ee7ba46e265ccdd4e0a8890c978eee776
steind29 <danielstein2018@u.northwestern.edu>
2018-06-04 20:00:19 -0500

    context attempt pt 1
    asdf

--- context.py
+++ context.py
@@ -1,5 +1,7 @@
 import sportReference as sr
-import queue
+import Queue
+import driver 
+import parsingTools as pt 
 
 class ContextFrame:
 
@@ -16,21 +18,59 @@ class ContextFrame:
 
 class ContextTracker:
 
-    frameTracker = queue.LifoQueue()
+    frameTracker = Queue.LifoQueue()
 
-    def __init__(self, sportRefs):
-        self.sportRefs = sportRefs
+    def __init__(self): #might have to re-add sportRefs as parameter
+        #self.sportRefs = sportRefs
         self.currFrame = None
 
-
+        #if we add sports, add locations
     def new_frame(self, sport = None, team1 = None, team2 = None, location1 = None, location2 = None, stat = None, player = None, date = None):
         newFrame = ContextFrame()
+
+        #first check driver to see if we can grab context from question.
+
+        
+        if driver.getDate() != None:
+            newFrame.date=driver.getDate()
+        elif driver.getSeason() != None:
+            newFrame.date = driver.getSeason()
+        #else look in past frames
+
+
+        if driver.getStats() != None:
+            newFrame.stat = driver.getStats()
+        #else look in past frames
+
+
+        if driver.getPlayer() != None:
+            newFrame.player = driver.getPlayer()
+        #find team of player, set team as well
+        #else look in past frames
+
+
+        if driver.getTeam() != None:
+            newFrame.team1 = driver.getTeams()[0]
+            if driver.getTeams()[1] !=None:
+                newFrame.team2 = driver.getTeams()[1]
+        #else look at players, fill in from roster (player team = team1)
+        #else look at past contexts
+
+
+
+
+
 
         # Assign values to all of newFrame's member variables here
         # self.currFrame refers to the most recent frame we've seen
 
         self.currFrame = newFrame
         self.pushFrame()
+        print("Help")
+        print(currFrame)
+
+    #def addContext():
+        #if we cant answer the question, look in past context frames
 
 
     def pushFrame(self):
@@ -38,4 +78,5 @@ class ContextTracker:
             self.frameTracker.get()
 
         self.frameTracker.put(self.currFrame)
+
 
